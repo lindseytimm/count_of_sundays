6f50f223292d0e5765db9cb9470d05b825a7cd90
Lindsey Timm <lindseytimm@potski.com>
2014-07-28 22:28:36 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountOfSundays::Application.config.secret_token = '83e30b463593adc5a556b6c9eb6b17f27e183274b0646aeb00c4fffabeb0b3fe6db28dadb7928392f1391a881d9b41e569a85c2c15bfd89ecb34171dc4a8f7f8'
